6743b764e29947cb62721ae0e04f680c4296fc27
Lawrencia Mensah <lawrencia.mensah@st.vvu.edu.gh>
2017-04-05 12:32:01 -0700

    creation of nav bar and layout pages

---
+++ blog/app/Http/Controllers/pagesController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+
+class pagesController extends Controller{
+public function getIndex(){
+
+return "Home";
+}
+
+public function getabout(){
+
+}
+
+
+public function getcontacts(){
+
+return "Hello contact page";
+
+
+}
+
+
+
+}
